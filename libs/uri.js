12319101077836d1d605c044c5e7a278a1c72c11
moved to @parknich081 <93056833+parknich082@users.noreply.github.com>
2022-09-27 10:46:44 -0400

    nsd fakjosdfn sdfa

---
+++ libs/uri.js
@@ -0,0 +1,7 @@
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null) {
+       return null;
+    }
+    return decodeURI(results[1]) || 0;
+}
